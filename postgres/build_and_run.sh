f18b14ba50e10b5236d26bc1bf5a8bbe67074c82
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-10-07 09:33:45 +0630

    run with postgres

--- postgres/build_and_run.sh
+++ postgres/build_and_run.sh
@@ -16,4 +16,39 @@ if [ $? -eq 0 ]; then
     echo "PostgreSQL container started successfully."
 else
     echo "Failed to start PostgreSQL container."
+    exit 1
 fi
+
+# Wait a few seconds to ensure the PostgreSQL service is fully up
+echo "Waiting for PostgreSQL service to initialize..."
+# sleep 10
+
+
+# Get the container ID based on the correct service name
+CONTAINER_NAME=$(docker compose ps -q postgres) # Use the correct service name, 'postgres'
+if [ -n "$CONTAINER_NAME" ]; then
+    echo "Copying database.sql to the container..."
+    docker cp database.sql "$CONTAINER_NAME":/database.sql
+
+    # Execute database.sql in the PostgreSQL container
+    echo "Executing database.sql in the container..."
+    docker exec -i "$CONTAINER_NAME" pg_restore --no-owner -U admin -d deeplink /database.sql
+    if [ $? -eq 0 ]; then
+        echo "SQL file executed successfully."
+    else
+        echo "Failed to execute SQL file."
+    fi
+else
+    echo "Failed to find the PostgreSQL container."
+    exit 1
+fi
+
+POSTGRES_USER="admin" 
+POSTGRES_PASSWORD="DN9z6JLAen728UpYFnyfKYEInALxykKUvYCWGz7EQueScSpEnu" 
+POSTGRES_DB="deeplink"
+POSTGRES_HOST="localhost"
+POSTGRES_PORT=$(docker compose port postgres 5432 | cut -d: -f2) # Extract the mapped port
+
+# Output the connection string
+echo "Your PostgreSQL connection string is ready to use:"
+echo "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
